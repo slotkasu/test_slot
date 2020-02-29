6e4f6e650f1f46f0bbf3ccc05b5c9c1bf2b7ad4a
xginga <xginga999@yahoo.co.jp>
2020-03-01 00:35:19 +0900

    Update trainPachinkoLSTM.py

--- pachinko/trainPachinkoLSTM.py
+++ pachinko/trainPachinkoLSTM.py
@@ -3,6 +3,15 @@
 from estimate_setting import getSettingList
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
 
 #print(getSettingList(40,35,8000))
 
@@ -37,6 +46,17 @@ single_setting = single_setting.reshape([len(setting),1])
 print(single_setting.shape)
 print(setting.shape)
 dataset=np.concatenate([single_setting, setting],1)
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+
 dataset=create_dataset(dataset, 30)
 print(dataset)
 
