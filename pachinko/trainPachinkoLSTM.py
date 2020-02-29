287493a8c1f16df9a655e8d5be82cb622877a0cd
Yuto Mukai <xginga999@yahoo.co.jp>
2020-02-29 22:42:49 +0900

    Update trainPachinkoLSTM.py

--- pachinko/trainPachinkoLSTM.py
+++ pachinko/trainPachinkoLSTM.py
@@ -12,11 +12,13 @@ with open('data/feb.csv') as f:
     data = [row for row in reader]
 
 #360番で実験　別にどれでもいい
-data360 = [i for i in data if i[0] == "360"]
+data360 = [i for i in data if i[0] == "366"]
 
 #BB RB TotalGames
 setting=[getSettingList(int(i[2]),int(i[3]),int(i[1])) for i in data360]
 
 setting = np.array(setting,dtype='float32')
+
+#0～5なので、設定値の1～6に変更する。
 single_setting = np.argmax(setting, axis=1)+1
 print(single_setting)
