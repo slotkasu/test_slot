04b5de4a102fdeaa03035a15a373a816b2b2533d
slotkasu <45270712+slotkasu@users.noreply.github.com>
2020-03-08 23:18:23 +0900

    csvたち

--- netkeiba/keiba_function.py
+++ netkeiba/keiba_function.py
@@ -3,70 +3,84 @@ from bs4 import BeautifulSoup
 
 #レース名を引数に番号を返す
 def getRaceNum(raceName):
-    race=["札幌","函館","福島","新潟","東京","中山","中京","京都","阪神","小倉"]
-    if raceName in race:
-        return str(race.index(raceName)+1)
-    else:
-        return "-1"
+	race=["札幌","函館","福島","新潟","東京","中山","中京","京都","阪神","小倉"]
+	if raceName in race:
+		return str(race.index(raceName)+1)
+	else:
+		return "-1"
 
 #芝ダを引数に番号を返す
 def getShibadaNum(shibadaName):
-    shibada=["芝","ダ"]
-    if shibadaName in shibada:
-        return str(shibada.index(shibadaName))
-    else:
-        return "-1"
+	shibada=["芝","ダ"]
+	if shibadaName in shibada:
+		return str(shibada.index(shibadaName))
+	else:
+		return "-1"
 
 #性別を引数に番号を返す
 def getSexNum(sexName):
-    sex=["牡","牝","セ"]
-    if sexName in sex:
-        return str(sex.index(sexName))
-    else:
-        return "-1"
+	sex=["牡","牝","セ"]
+	if sexName in sex:
+		return str(sex.index(sexName))
+	else:
+		return "-1"
 
 #馬場状態を引数に番号を返す
 def getStateNum(stateName):
-    state=["良","稍","重","不"]
-    if stateName in state:
-        return str(state.index(stateName))
-    else:
-        return "-1"
+	state=["良","稍","重","不"]
+	if stateName in state:
+		return str(state.index(stateName))
+	else:
+		return "-1"
 
 
 
 #馬名、着順、オッズをリストで返す
 def getRaceResult(day):
-    url = "https://race.netkeiba.com/race/result.html?race_id="+day+"&rf=race_submenu"
-    html = requests.get(url)
-    html.encoding = html.apparent_encoding
-    soup = BeautifulSoup(html.content,'html.parser')
-    name = soup.find_all("tr", class_="HorseList")
+	url = "https://race.netkeiba.com/race/result.html?race_id="+day+"&rf=race_submenu"
+	html = requests.get(url)
+	html.encoding = html.apparent_encoding
+	soup = BeautifulSoup(html.content,'html.parser')
+	name = soup.find_all("tr", class_="HorseList")
 
-    results = []
+	results = []
 
-    for na in name:
-        #2重リスト用
-        temp = []
-        #馬名
-        horse = na.find("span", class_="Horse_Name")
-        temp.append(horse.text.strip())
-        #着順
-        rank = na.find("div", class_="Rank")
-        temp.append(rank.text.strip())
-        #オッズ
-        odds = na.find("td", class_="Odds Txt_R")
-        temp.append(odds.text.strip())
-        #馬番（ソート用）
-        umaban = na.find("td", class_="Num Txt_C")
-        temp.append(int(umaban.text.strip()))
-        results.append(temp)
-    
+	for na in name:
+		#2重リスト用
+		temp = []
+		#馬名
+		horse = na.find("span", class_="Horse_Name")
+		temp.append(horse.text.strip())
+		#着順
+		rank = na.find("div", class_="Rank")
+		temp.append(rank.text.strip())
+		#オッズ
+		odds = na.find("td", class_="Odds Txt_R")
+		temp.append(odds.text.strip())
+		#馬番（ソート用）
+		umaban = na.find("td", class_="Num Txt_C")
+		temp.append(int(umaban.text.strip()))
+		results.append(temp)
+	
 
-    results.sort(key=lambda x: x[3])#2列めを基準にソート
+	results.sort(key=lambda x: x[3])#2列めを基準にソート
 
-    #ソートに利用した馬番を削除
-    for i in results:
-        i.pop(3)
+	#ソートに利用した馬番を削除
+	for i in results:
+		i.pop(3)
 
-    return results
+	return results
+
+
+#2:00.0みたいなやつを秒数にする
+def TtoF(time_origin):#stringでください
+	ls = time_origin.split(":",1)
+	if len(ls) == 1:
+		ls = []
+		ls = time_origin.split(".",1)
+	if len(ls) == 1:
+		return time_origin
+	min = float(ls[0])
+	sec = float(ls[1])
+	tt = min*60 + sec
+	return str(tt)
--- netkeiba/timetofloat.py
+++ netkeiba/timetofloat.py
@@ -1,7 +1,9 @@
 import numpy as np
 import re
 import csv
+import glob
 
+#string time_origin to string tt
 def TtoF(time_origin):
 	ls = time_origin.split(":",1)
 	if len(ls) == 1:
@@ -9,7 +11,7 @@ def TtoF(time_origin):
 	min = float(ls[0])
 	sec = float(ls[1])
 	tt = min*60 + sec
-	return tt
+	return str(tt)
 
 def getCsv(filename):#csv wo tottekuru
 #16+18+18 kono kankaku
@@ -24,13 +26,14 @@ def getCsv(filename):#csv wo tottekuru
 		row[87] = TtoF(row[87])
 		timeAlt.append(row)
 	f.close()
-	f = open('test.csv','w',newline = "")
+	f = open(filename,'w',newline = "")
 	writer = csv.writer(f)
 	writer.writerows(timeAlt)
 	f.close()
 
-paths = glob.glob("datasets\\"+year+month+"\\*")
+paths = glob.glob("keiba\\datasets\\"+"*")
 
-getCsv("201906030403out.csv")
+for i in paths:
+	getCsv(i)
 
 
