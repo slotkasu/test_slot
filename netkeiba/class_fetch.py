9518731aeae8ff369305c11efa268fa0ed58f06c
Yuto Mukai <xginga999@yahoo.co.jp>
2020-03-08 10:14:53 +0900

    Update class_fetch.py

--- netkeiba/class_fetch.py
+++ netkeiba/class_fetch.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import warnings
 import csv
 import pprint
-import time
+import tim
 import datetime
 from datetime import timedelta
 
@@ -15,11 +15,15 @@ url = "https://race.netkeiba.com/race/shutuba_past.html?race_id=201906020111&rf=
 html = requests.get(url)
 html.encoding = html.apparent_encoding
 soup = BeautifulSoup(html.content,'html.parser')
-# name = soup.find_all("td")
-# div = name.find_all("div")
 
-name = soup.select("td div.Horse04")
+#trタグのHorseListクラスからtr_[0-9]{2}のものだけを抽出
+name = soup.find_all("tr",class_="HorseList",id=re.compile('tr_[0-9]+'))
 
+for na in name:	
+	divs = na.find_all("div")
+	for div in na.find_all("div"):
+		print(div.text.strip())
+	print("\n_________________________")
 
-for i in name:
-	print(i.text)
+# for i in name:
+# 	print(i.text)
