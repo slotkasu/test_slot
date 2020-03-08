f5860543c90dc2250463f0b3607014121faa7045
slotkasu <45270712+slotkasu@users.noreply.github.com>
2020-03-08 16:11:07 +0900

    function_append

--- netkeiba/keiba_function.py
+++ netkeiba/keiba_function.py
@@ -1,4 +1,5 @@
-
+import requests
+from bs4 import BeautifulSoup
 
 #レース名を引数に番号を返す
 def getRaceNum(raceName):
@@ -34,3 +35,39 @@ def getStateNum(stateName):
 
 
 
+#馬名、着順、オッズをリストで返す
+def getRaceResult(day):
+    url = "https://race.netkeiba.com/race/result.html?race_id="+day+"&rf=race_submenu"
+    html = requests.get(url)
+    html.encoding = html.apparent_encoding
+    soup = BeautifulSoup(html.content,'html.parser')
+    name = soup.find_all("tr", class_="HorseList")
+
+    results = []
+
+    for na in name:
+        #2重リスト用
+        temp = []
+        #馬名
+        horse = na.find("span", class_="Horse_Name")
+        temp.append(horse.text.strip())
+        #オッズ
+        odds = na.find("td", class_="Odds Txt_R")
+        temp.append(odds.text.strip())
+        #着順
+        rank = na.find("div", class_="Rank")
+        temp.append(rank.text.strip())
+        #馬番（ソート用）
+        umaban = na.find("td", class_="Num Txt_C")
+        temp.append(int(umaban.text.strip()))
+        results.append(temp)
+    
+
+    results.sort(key=lambda x: x[3])#2列めを基準にソート
+
+    for i in results:
+        i.pop(3)
+
+    for i in results:
+        print(i)
+    return results
