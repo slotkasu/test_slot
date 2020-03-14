20179693b26b25df2b6a4b7a4564cd18e44ae235
slotkasu <45270712+slotkasu@users.noreply.github.com>
2020-03-14 22:46:13 +0900

    Update train_keiba_data.py

--- netkeiba/train_keiba_data.py
+++ netkeiba/train_keiba_data.py
@@ -109,7 +109,7 @@ model.compile(loss='categorical_crossentropy',
 epochs=1000
 
 history = model.fit(X_train, Y_train,
-                    batch_size=256,
+                    batch_size=1024,
                     epochs=epochs,
                     verbose=1,
                     validation_data=(X_test, Y_test))
