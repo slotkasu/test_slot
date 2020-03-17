4b236c63bae37f2bd0f40c84d22c107476b710cd
slotkasu <45270712+slotkasu@users.noreply.github.com>
2020-03-17 23:57:32 +0900

    123123

--- netkeiba/raceid.py
+++ netkeiba/raceid.py
@@ -17,7 +17,7 @@ date = datetime.datetime.now()
 course_list = [str(i+1).zfill(2) for i in range(10)]
 kaisai_list = [str(i+1).zfill(2) for i in range(4)]
 date_list = [str(i+1).zfill(2) for i in range(9)]
-race_list = [str(i+1).zfill(2) for i in range(12)]
+race_list = [str(i+7).zfill(2) for i in range(6)]
 
 #この番号からはじめる
 skip = year + "08040202"
