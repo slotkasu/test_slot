dafaa6c9317ef6d125706f5e973c5d8252b3a7d3
slotkasu <45270712+slotkasu@users.noreply.github.com>
2020-03-26 21:40:35 +0900

    12

--- netkeiba/raceid.py
+++ netkeiba/raceid.py
@@ -17,12 +17,12 @@ date = datetime.datetime.now()
 course_list = [str(i+1).zfill(2) for i in range(10)]
 kaisai_list = [str(i+1).zfill(2) for i in range(4)]
 date_list = [str(i+1).zfill(2) for i in range(9)]
-# race_list = [str(i+7).zfill(2) for i in range(6)]
-race_list = [str(i+1).zfill(2) for i in range(6)]
+race_list = [str(i+7).zfill(2) for i in range(6)]
+# race_list = [str(i+1).zfill(2) for i in range(6)]
 
 #この番号からはじめる　8桁
 
-skip = year + "00000000" #"00000000"
+skip = year + "05040211" #"00000000"
 
 for course in course_list:
 	for kaisai in kaisai_list:
