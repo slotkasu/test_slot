6c2665d97570e747f76f722267bff6886de615e1
xginga <xginga999@yahoo.co.jp>
2020-09-06 17:51:54 +0900

    a
     a

--- slot/showGraphic_slot.py
+++ slot/showGraphic_slot.py
@@ -1,7 +1,9 @@
 import wx
 import numpy as np
+# import slot_sample
 
 class SlotApp(wx.Frame):
+
 	def __init__(self, parent, id = -1, title = 'Window Title'):
 		wx.Frame.__init__(self, parent, id, title)
 		self.SetSize((500,200))
@@ -9,7 +11,7 @@ class SlotApp(wx.Frame):
 		panel = wx.Panel(self)
 
 		#フラグ表示用処理
-		self.flag_text = wx.StaticText(panel,wx.ID_ANY,'S牙狼～Type-A～',style=wx.TE_CENTER)
+		self.flag_text = wx.StaticText(panel,wx.ID_ANY,'小役',style=wx.TE_CENTER)
 		font=wx.Font(30, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
 		self.flag_text.SetFont(font)
 
@@ -17,19 +19,24 @@ class SlotApp(wx.Frame):
 		self.startButton=wx.Button(panel, label="Start")
 
 		#ボタン割り当て
-		self.Bind(wx.EVT_BUTTON)
+		self.Bind(wx.EVT_BUTTON, self.startLottery, self.startButton)
 		
 		#レイアウト設定
 		layout = wx.BoxSizer(wx.VERTICAL)
 		layout.Add(self.flag_text,flag=wx.GROW)
 		layout.Add(self.startButton,flag=wx.ALIGN_CENTER | wx.BOTTOM | wx.RIGHT, border=10)
-
 		panel.SetSizer(layout)
 
+		#表示処理
 		self.Center()
 		self.Show()
+
+	def startLottery(self,event):
+		self.flag_text.SetLabel("テスト")
+
+
 		
 
 app=wx.App()
-SlotApp(None,id=wx.ID_ANY,title="スロットくん")
+SlotApp(None,id=wx.ID_ANY,title="S牙狼魔戒ノ花XX")
 app.MainLoop()
